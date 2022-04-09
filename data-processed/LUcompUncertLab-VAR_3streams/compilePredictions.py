109be4279cbee1a1f540a76e2b524a9084547d72
Matthew Piriya <piriya.matt@gmail.com>
2022-04-09 15:42:06 -0400

    no longer is the final file

--- data-processed/LUcompUncertLab-VAR_3streams/compilePredictions.py
+++ data-processed/LUcompUncertLab-VAR_3streams/compilePredictions.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 if __name__ == "__main__":
 	io = interface(0)
-	final_filename = "{:s}-LUcompUncertLab-VAR_3streams.csv".format(io.forecast_date)
+	almost_final_filename = "{:s}-LUcompUncertLab-VAR_3streams__ALMOSTFINAL.csv".format(io.forecast_date)
 
-	for n, file in enumerate(glob('./location_specific_forecasts/{:s}_LUcompUncertLab-quantiles*'.format(io.forecast_date))):
+	for n, file in enumerate(glob('./location_specific_forecasts/{:s}_LUcompUncertLab-VAR3Streams_FINAL*'.format(io.forecast_date))):
 		curr = pd.read_csv(file, compression='gzip')
 		if n == 0:
-			curr.to_csv(final_filename, index=False)
+			curr.to_csv(almost_final_filename, index=False)
 		else:
-			curr.to_csv(final_filename, index=False, mode='a', header=False)
+			curr.to_csv(almost_final_filename, index=False, mode='a', header=False)
