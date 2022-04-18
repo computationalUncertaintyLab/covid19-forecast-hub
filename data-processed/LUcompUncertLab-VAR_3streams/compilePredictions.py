30d23c148350c398830f0af54b36767bcf455d64
tom mcandrew <mcandrew@lehigh.edu>
2022-04-17 22:04:55 -0400

    tab error fix

--- data-processed/LUcompUncertLab-VAR_3streams/compilePredictions.py
+++ data-processed/LUcompUncertLab-VAR_3streams/compilePredictions.py
@@ -9,14 +9,15 @@ import pandas as pd
 import datetime
 
 if __name__ == "__main__":
-	io = interface(0)
 
-        monday = (pd.to_datetime(io.forecast_date) + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-	almost_final_filename = "{:s}-LUcompUncertLab-VAR_3streams__ALMOSTFINAL.csv".format(monday)
-
-	for n, file in enumerate(glob('./location_specific_forecasts/{:s}_LUcompUncertLab-VAR3Streams_FINAL*'.format(io.forecast_date))):
-		curr = pd.read_csv(file, compression='gzip')
-		if n == 0:
-			curr.to_csv(almost_final_filename, index=False)
-		else:
-			curr.to_csv(almost_final_filename, index=False, mode='a', header=False)
+    io = interface(0)
+        
+    monday = (pd.to_datetime(io.forecast_date) + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    almost_final_filename = "{:s}-LUcompUncertLab-VAR_3streams__ALMOSTFINAL.csv".format(monday)
+        
+    for n, file in enumerate(glob('./location_specific_forecasts/{:s}_LUcompUncertLab-VAR3Streams_FINAL*'.format(monday))):
+        curr = pd.read_csv(file, compression='gzip')
+        if n == 0:
+            curr.to_csv(almost_final_filename, index=False)
+        else:
+            curr.to_csv(almost_final_filename, index=False, mode='a', header=False)
--- data-processed/LUcompUncertLab-VAR_3streams/postpostprocess.py
+++ data-processed/LUcompUncertLab-VAR_3streams/postpostprocess.py
@@ -13,45 +13,46 @@ import pandas as pd
 import datetime
 
 if __name__ == "__main__":
-        io = interface(0)
+    io = interface(0)
 
-        monday = (pd.to_datetime(io.forecast_date) + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    monday = (pd.to_datetime(io.forecast_date) + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
 
-	final_file = "{:s}-LUcompUncertLab-VAR_3streams.csv".format(monday)
-	file = "{:s}-LUcompUncertLab-VAR_3streams__ALMOSTFINAL.csv".format(monday)
-	# read in csv
-	allPredictions = pd.read_csv(file)
-	
-	# normalize location values
-	newloc = []
-	for x in allPredictions['location']:
-		if(len(str(x)) > 2):
-			newloc.append(str(x).zfill(5))
-		else:
-			newloc.append(str(x).zfill(2))
+    final_file = "{:s}-LUcompUncertLab-VAR_3streams.csv".format(monday)
+    file = "{:s}-LUcompUncertLab-VAR_3streams__ALMOSTFINAL.csv".format(monday)
+    # read in csv
+    allPredictions = pd.read_csv(file)
 
-	s = pd.Series(newloc)
-	allPredictions['location'] = s
+    # normalize location values
+    newloc = []
+    for x in allPredictions['location']:
+        if(len(str(x)) > 2):
+            newloc.append(str(x).zfill(5))
+        else:
+            newloc.append(str(x).zfill(2))
 
-	# TEMPORARY fix for date values (so i don't have to run cluster job again)
-	# allPredictions['target_end_date'] = pd.to_datetime(allPredictions['target_end_date']) + pd.DateOffset(days=2)
-	# allPredictions['target_end_date'] = allPredictions.target_end_date.dt.strftime("%Y-%m-%d")
-	
-	# normalize target values
-	allPredictions['target'] = allPredictions['target'].str.replace("covid ", "", regex=False)
-	allPredictions['target'] = allPredictions['target'].str.replace("week", "wk", regex=False)
 
-	# take out extraneous targets
-	toSubmit = {"target":[], "location":[], "quantile": []}
-	wk_inc_case_quantiles = [0.025, 0.100, 0.250, 0.500, 0.750, 0.900, 0.975]
+    s = pd.Series(newloc)
+    allPredictions['location'] = s
 
-	for row in allPredictions.itertuples(index=False):
-		if (len(str(row.location)) < 3 and ("wk ahead cum death" in row.target or "wk ahead inc death" in row.target or "day ahead inc hosp" in row.target)) or ("wk ahead inc case" in row.target and row.quantile in wk_inc_case_quantiles):
-			toSubmit["target"].append(row.target)
-			toSubmit["location"].append(row.location)
-			toSubmit["quantile"].append(row.quantile)
+    # TEMPORARY fix for date values (so i don't have to run cluster job again)
+    # allPredictions['target_end_date'] = pd.to_datetime(allPredictions['target_end_date']) + pd.DateOffset(days=2)
+    # allPredictions['target_end_date'] = allPredictions.target_end_date.dt.strftime("%Y-%m-%d")
 
-	toSubmit = pd.DataFrame(toSubmit)
-	toSubmit = toSubmit.merge(allPredictions, on=["target", "location", "quantile"], how="left")
+    # normalize target values
+    allPredictions['target'] = allPredictions['target'].str.replace("covid ", "", regex=False)
+    allPredictions['target'] = allPredictions['target'].str.replace("week", "wk", regex=False)
 
-	toSubmit.to_csv(final_file, index=False)
+    # take out extraneous targets
+    toSubmit = {"target":[], "location":[], "quantile": []}
+    wk_inc_case_quantiles = [0.025, 0.100, 0.250, 0.500, 0.750, 0.900, 0.975]
+
+    for row in allPredictions.itertuples(index=False):
+        if (len(str(row.location)) < 3 and ("wk ahead cum death" in row.target or "wk ahead inc death" in row.target or "day ahead inc hosp" in row.target)) or ("wk ahead inc case" in row.target and row.quantile in wk_inc_case_quantiles):
+            toSubmit["target"].append(row.target)
+            toSubmit["location"].append(row.location)
+            toSubmit["quantile"].append(row.quantile)
+
+    toSubmit = pd.DataFrame(toSubmit)
+    toSubmit = toSubmit.merge(allPredictions, on=["target", "location", "quantile"], how="left")
+
+    toSubmit.to_csv(final_file, index=False)
