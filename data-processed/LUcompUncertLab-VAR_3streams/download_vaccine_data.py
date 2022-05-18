2e089cf591e4a68bbd83beb09990124f27856997
tom mcandrew <mcandrew@lehigh.edu>
2022-05-18 17:34:37 -0400

    Try number 3 to patch

--- data-processed/LUcompUncertLab-VAR_3streams/download_vaccine_data.py
+++ data-processed/LUcompUncertLab-VAR_3streams/download_vaccine_data.py
@@ -10,15 +10,15 @@ if __name__ == "__main__":
 
     client = Socrata("data.cdc.gov", None)
 
-    results = client.get("unsk-b7fc",limit=10**4,offset=0)
+    results = client.get("8xkx-amqh",limit=10**4,offset=0)
     allData = pd.DataFrame(results)
 
     n=1
     while results !=[]:
-        results = client.get("unsk-b7fc",limit=10**4,offset=n*10**4)
+        results = client.get("8xkx-amqh",limit=3*10**4,offset=n*10**4)
         resultsData = pd.DataFrame(results)
         
         allData = allData.append( resultsData )
 
         n+=1
-    allData.to_csv("allVaccinationData.csv")
+    allData.to_csv("allVaccinationData.csv.gz",compression="gzip")
