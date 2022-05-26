b2728e6ee417583ec49bd89921ac55fad409d659
Abraham Berlin <alb323@lehigh.edu>
2022-05-26 15:44:39 -0400

    Can now append new data to old data

--- data-processed/LUcompUncertLab-VAR_3streams/getGoogleData.py
+++ data-processed/LUcompUncertLab-VAR_3streams/getGoogleData.py
@@ -4,14 +4,19 @@ from dataclasses import dataclass
 import numpy as np
 import pandas as pd
 
-from datetime import date
+from datetime import date, datetime, timedelta
 
 import covidcast
 
 
 if __name__ == "__main__":
 
-    county = covidcast.signal(data_source="google-symptoms",signal="s01_raw_search", geo_type="county", start_day = date(2020,1,22))
+
+    google = pd.read_csv("./AllGoogleData.csv")
+
+    start_date = datetime.strptime(max(google["date"]), '%Y-%m-%d').date() + timedelta(days=1)
+
+    county = covidcast.signal(data_source="google-symptoms",signal="s01_raw_search", geo_type="county", start_day = start_date)
 
     data = pd.DataFrame()
     data["date"] = county["time_value"]
@@ -19,7 +24,7 @@ if __name__ == "__main__":
     data["location_name"] = covidcast.fips_to_name(data["location"])
     data["value"] = county["value"]
 
-    state = covidcast.signal(data_source="google-symptoms",signal="s01_raw_search", geo_type="state", start_day = date(2020,1,22))
+    state = covidcast.signal(data_source="google-symptoms",signal="s01_raw_search", geo_type="state", start_day = start_date)
 
     data2 = pd.DataFrame()
     data2["date"] = state["time_value"]
@@ -27,7 +32,7 @@ if __name__ == "__main__":
     data2["location_name"] = covidcast.abbr_to_name(state["geo_value"], ignore_case=True)
     data2["value"] = state["value"]
 
-    data = data.append(data2)
+    data = pd.concat([google, data, data2], ignore_index=True)
 
     data.to_csv("AllGoogleData.csv", index=False)
 
