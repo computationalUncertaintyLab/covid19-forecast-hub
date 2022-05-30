cec026d08ed1fb76915e954aa80ea114f89108a1
Abraham Berlin <alb323@lehigh.edu>
2022-05-29 22:16:55 -0400

    Fill in Missing Google Data

--- data-processed/LUcompUncertLab-VAR_3streams/getGoogleData.py
+++ data-processed/LUcompUncertLab-VAR_3streams/getGoogleData.py
@@ -32,7 +32,40 @@ if __name__ == "__main__":
     data2["location_name"] = covidcast.abbr_to_name(state["geo_value"], ignore_case=True)
     data2["value"] = state["value"]
 
-    data = pd.concat([google, data, data2], ignore_index=True)
+    google = pd.concat([google, data, data2], ignore_index=True)
+
+    cases = pd.read_csv("../../data-truth/truth-Incident Cases.csv")
+    cases_locations = pd.Index(cases["location"].unique())
+
+    dic = {"date":[], "location":[], "location_name":[], "value":[]}
+    for d in google["date"].unique():
+        # loc to date
+        sub_google = google.loc[google["date"] == d]
+
+        # aggregate all states
+        mask = (sub_google["location"].str.len() <= 2) & (sub_google["location"] != "US")
+        state = sub_google.loc[mask]
+        state["location"] = state["location"].astype(int)
+        
+        # add to df
+        dic["date"].append(date)
+        dic["location"].append("US")
+        dic["location_name"].append("US")
+        dic["value"].append(state["value"].mean())
+
+
+        google_locations = pd.Index(sub_google["location"].unique())
+        diff = cases_locations.difference(google_locations).tolist()
+        
+        
+        for l in diff:
+            value = state.loc[state["location"] == int(l/1000)].iat[0,3]
+            dic["date"].append(d)
+            dic["location"].append(str(l))
+            dic["location_name"].append(covidcast.fips_to_name(str(l)))
+            dic["value"].append(value)
+
+    data = pd.concat([google, pd.DataFrame.from_dict(dic)], ignore_index=True)
 
     data.to_csv("AllGoogleData.csv", index=False)
 
