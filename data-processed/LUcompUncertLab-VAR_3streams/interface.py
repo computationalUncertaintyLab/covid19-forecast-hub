7ef0ffa73954692934558afcd2858b761e5d6f0d
Abraham Berlin <alb323@lehigh.edu>
2022-06-07 12:09:06 -0500

    Four Streams Alpha

--- data-processed/LUcompUncertLab-VAR_3streams/interface.py
+++ data-processed/LUcompUncertLab-VAR_3streams/interface.py
@@ -132,9 +132,9 @@ class interface(object):
     def generateTargetNames(self):
         import numpy as np
 
-        # first target is always cases, second deaths, and third hosps.
+        # first target is always cases, second deaths, third hosp, and fourth google
         targets = []
-        trgts = ["case","death","hosp"]
+        trgts = ["case","death","hosp","google"]
         for trgt in trgts:
             targets.append(["{:d} day ahead inc covid {:s}".format(ahead,trgt) for ahead in np.arange(1,29+1)])
 
@@ -158,6 +158,7 @@ class interface(object):
                 dataPredictions["forecast_date"].extend(F*[self.forecast_date])
                 dataPredictions["location"].extend( F*[self.location] )
                 dataPredictions["target_end_date"].extend( self.target_end_days )
+                print(n)
                 dataPredictions["target"].extend( self.targets[n] )
                 dataPredictions["sample"].extend( F*[sample] )
                 dataPredictions["value"].extend( forecast )
@@ -247,7 +248,7 @@ class interface(object):
     def grab_post_process_predictions(self):
         from glob import glob
         import pandas as pd
-        files = sorted(glob("./location_specific_forecasts/*LUcompUncertLab-VAR3Streams__{:s}.csv.gz".format(self.fmtlocation)))
+        files = sorted(glob("./location_specific_forecasts/*LUcompUncertLab-VAR4Streams__{:s}.csv.gz".format(self.fmtlocation)))
 
         self.predictions = pd.read_csv(files[-1])
         return self.predictions
@@ -256,7 +257,7 @@ class interface(object):
         from glob import glob
         import pandas as pd
 
-        files = sorted(glob("./location_specific_forecasts/*LUcompUncertLab-VAR3Streams_FINAL__{:s}.csv.gz".format(self.fmtlocation)))
+        files = sorted(glob("./location_specific_forecasts/*LUcompUncertLab-VAR4Streams_FINAL__{:s}.csv.gz".format(self.fmtlocation)))
 
         self.quantiles = pd.read_csv(files[-1])
         return self.quantiles
