991a7af8f709944a94281d7e0e0ac1a2cba22c8d
Parth github <98370984+parthdg420@users.noreply.github.com>
2022-06-17 11:47:30 -0400

    fourstreas changes

--- data-processed/LUcompUncertLab-VAR_3streams/Fourstreams/data-processing.py
+++ data-processed/LUcompUncertLab-VAR_3streams/Fourstreams/data-processing.py
@@ -8,13 +8,14 @@ class dataprep(object):
         self.load_cases()
         self.load_deaths()
         self.load_hosps()
+        self.load_vac_count()
         self.merge()
         self.mergeCounties()
         self.combineStateAndCounty()
         
     def load_cases(self):
         import pandas as pd
-        cases = pd.read_csv("../../data-truth/truth-Incident Cases.csv")
+        cases = pd.read_csv("truth-Incident Cases.csv")
         cases.location = cases.location.astype(str)
         
         # we need to separate cases at the state level from cases at county level
@@ -43,7 +44,7 @@ class dataprep(object):
         
     def load_deaths(self):
         import pandas as pd
-        deaths = pd.read_csv("../../data-truth/truth-Incident Deaths.csv")
+        deaths = pd.read_csv("truth-Incident Deaths.csv")
         deaths.location = deaths.location.astype(str)
 
         norm_loc = []
@@ -61,7 +62,7 @@ class dataprep(object):
 
     def load_hosps(self):
         import pandas as pd
-        hosps = pd.read_csv("../../data-truth/truth-Incident Hospitalizations.csv")
+        hosps = pd.read_csv("truth-Incident Hospitalizations.csv")
         hosps.location = hosps.location.astype(str)
 
         norm_loc = []
@@ -76,12 +77,36 @@ class dataprep(object):
 
         self.hosps = hosps
 
+    def load_vac_count(self):
+        import pandas as pd
+        vac_count = pd.read_csv('_4streams.csv.gz')
+        vac_count = vac_count[['date','location','location_name','vac_count']]
+
+        vac_count.location = vac_count.location.astype(str)
+        vac_counties = vac_count.loc[ [True if len(_)>=4 else False for _ in vac_count.location] ,:]
+        norm_loc = []
+        for l in vac_count.location:
+            if len(l) == 2:
+                norm_loc.append(l)
+            else:
+                norm_loc.append(l.zfill(2))
+        vac_count.location = norm_loc
+        self.vac_count = vac_count
+        # strips the last three digits from the FIPS to get either 1 or 2 digits, then left fills with 0 if 1 digit
+        state_codes = vac_counties.apply(lambda x: x['location'][:-3].zfill(2), axis=1)
+
+        # appends to the dataframe
+        vac_counties.insert(len(vac_counties.columns), 'state', state_codes)
+
+        self.vac_counties = vac_counties
+        
+
     def merge(self):
         key = ["date","location","location_name"]
         d = self.cases.merge(self.deaths, on = key)
         d = d.merge(self.hosps, on = key )
-
-        self.threestreams_state = d
+        d = d.merge(self.vac_count, on = key )
+        self.fourstreams_state = d
         return d
 
     def mergeCounties(self):
@@ -89,31 +114,33 @@ class dataprep(object):
         d = self.ccases_counties.merge(self.deaths, left_on=['date', 'state'], right_on=['date', 'location'], suffixes=(None, "_d"))
         # merge the county cases with state hosps
         d = d.merge(self.hosps, left_on=['date', 'state'], right_on=['date', 'location'], suffixes=(None, "_h"))
-        d = d.drop(columns=['state', 'location_d', 'location_name_d', 'location_h', 'location_name_h'])
+        d = d.merge(self.vac_count, left_on=['date','state'], right_on=['date','location'], suffixes=(None,"_v"))
+        d = d.drop(columns=['state', 'location_d', 'location_name_d', 'location_h', 'location_name_h','location_v','location_v'])
 
-        self.threestreams_county = d
+        self.fourstreams_county = d
         return d
 
     def combineStateAndCounty(self):
         import pandas as pd
-        # append county threestreams to state threestreams
-        d = pd.concat([self.threestreams_state, self.threestreams_county])
+        # append county fourstreams to state fourstreams
+        d = pd.concat([self.fourstreams_state, self.fourstreams_county])
 
         # store it in instance variable threestreams
-        self.threestreams = d
+        self.fourstreams = d
         
-        # rename columns in threestreams_county for clarity
-        self.threestreams_county = self.threestreams_county.rename(columns={"cases":"county_cases", "deaths":"state_deaths", "hosps":"state_hosps"})
+        # rename columns in fourstreams_county for clarity
+        self.fourstreams_county = self.fourstreams_county.rename(columns={"cases":"county_cases", "deaths":"state_deaths", "hosps":"state_hosps","vac_count":"county_vac_count"})
 
     def write(self):
         def tocsv(x,f):
             x.to_csv(f,index=False,compression = "gzip")
-        tocsv(self.threestreams_state,"threestreams__state.csv.gz")
-        tocsv(self.threestreams_county,"threestreams__county.csv.gz")
-        tocsv(self.threestreams,"threestreams.csv.gz")
+        tocsv(self.fourstreams_state,"fourstreams__state.csv.gz")
+        tocsv(self.fourstreams_county,"fourstreams__county.csv.gz")
+        tocsv(self.fourstreams,"fourstreams.csv.gz")
         tocsv(self.cases,"cases.csv.gz")
         tocsv(self.deaths,"deaths.csv.gz")
         tocsv(self.hosps,"hosps.csv.gz")
+        tocsv(self.vac_count,"vac_count.csv.gz")
         
 if __name__ == "__main__":
 
