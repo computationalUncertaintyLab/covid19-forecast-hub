13729e5bdd55c9ea9b29fc30930a96b838a547f7
Parth github <98370984+parthdg420@users.noreply.github.com>
2022-07-12 08:24:50 -0400

    files for model to run

---
+++ data-processed/LUcompUncertLab-VAR_3streams/state-Vaccinations.py
@@ -0,0 +1,80 @@
+ #working with the state level data
+from cmath import nan
+from heapq import merge
+import pandas as pd
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+if __name__ == "__main__":
+
+    vaccination_df= pd.read_csv('allVaccinationData.csv.gz')
+    vaccination_df = vaccination_df.loc[:, ["date","fips","recip_county", "recip_state", "series_complete_yes"]]
+    vaccination_df['state_fip'] = vaccination_df['fips'].str[:2]
+    vaccination_df['date'] = pd.to_datetime(vaccination_df['date'])
+    vaccination_df.rename(columns = {'date':'date','Recip_County':'location_name', 'fips':'location','series_complete_yes':'vac_count'}, inplace=True)
+    vaccination_df = vaccination_df[(vaccination_df['date'] >= '2020-12-13') & (vaccination_df['date'] <= '2022-04-29')]
+    lis1 = ['UN','66',nan]
+    vaccination_df_1 = vaccination_df[~vaccination_df['state_fip'].isin(lis1)]
+    vaccination_df_1.rename(columns = {'date':'date','recip_county':'location_name', 'FIPS':'location','Series_Complete_Yes':'Vacc_count','recip_state':'state_ab'}, inplace=True)
+    vaccination_df_state = vaccination_df_1.groupby(['state_ab','date'], as_index=False)['vac_count'].sum()
+    #so we have the vaccination count for each state on daily basis
+    #now we will be working with the locations
+    location_df = pd.read_csv('locations.csv') 
+    location_df_state = location_df[location_df['abbreviation'].notna()]
+    location_df_state['state_fip'] = location_df_state['location'].str[:2]
+    location_df_state = location_df_state[location_df_state['location_name']!='US']
+    merge1 = pd.merge(vaccination_df_state, location_df_state, how='left', left_on=['state_ab'], right_on=['abbreviation'])
+    merge1 = merge1.loc[:,['state_ab','date','vac_count','location_name','population','state_fip']]
+    print(merge1.head())
+    #now we need the threestreams 
+    threestreams_s = pd.read_csv('threestreams__state.csv.gz')
+    threestreams_s['date'] = pd.to_datetime(threestreams_s['date'])
+    threestreams_s = threestreams_s[(threestreams_s['date'] >= '2020-12-13') & (threestreams_s['date'] <= '2022-04-29')]
+    print(threestreams_s)
+    fourstreams_s = pd.merge(threestreams_s, merge1, how = 'left', left_on=['date','location','location_name'], right_on=['date','state_fip','location_name'])
+    #we dont have values for American Samoa and United states(i.e. it is on a national level)
+    #for american Samoa we replace the value by 1 and the est of the values were googled
+    fourstreams_s['vac_count'] = np.where((fourstreams_s['location_name'] == 'American Samoa'),1,fourstreams_s['vac_count'])
+    fourstreams_s['state_ab'] = np.where((fourstreams_s['location_name'] == 'American Samoa'),'AS',fourstreams_s['state_ab'])
+    fourstreams_s['state_fip'] = np.where((fourstreams_s['location_name'] == 'American Samoa'),60,fourstreams_s['state_fip'])
+    fourstreams_s['population'] = np.where((fourstreams_s['location_name'] == 'American Samoa'),55197.0,fourstreams_s['population'])   
+    #we need to work on everyday vac count at a daily scale
+    vaccination_df_country = vaccination_df_1.groupby('date', as_index=False)['vac_count'].sum()
+    fourstreams_s['population'] = np.where((fourstreams_s['location_name'] == 'United States'),332915073.0,fourstreams_s['population']) 
+    fourstreams_s['state_ab'] = np.where((fourstreams_s['location_name'] == 'United States'),'US',fourstreams_s['state_ab'])
+    fourstreams_s['state_fip'] = np.where((fourstreams_s['location_name'] == 'United States'),00,fourstreams_s['state_fip'])
+    #merging with the national daily vaccination count
+    fourstreams_s = pd.merge(fourstreams_s, vaccination_df_country, how='left', left_on=['date'], right_on=['date'])
+    fourstreams_s.rename(columns = {'vac_count_x':'vac_count','vac_count_y':'country_vac_count'}, inplace=True)
+    fourstreams_s['vac_count'] = np.where((fourstreams_s['location_name'] == 'United States'),fourstreams_s['country_vac_count'],fourstreams_s['vac_count'])
+    fourstreams_s.drop(columns=['country_vac_count'], inplace=True)
+    print(fourstreams_s.head())
+    print(fourstreams_s.nunique())
+    print(fourstreams_s.info())
+    print(len(fourstreams_s))
+    print(fourstreams_s.isna().sum())
+    #so our fourstream_s data is now ready to be added and it has no nan values
+<<<<<<< HEAD
+    fourstreams_s.drop("population", axis = 1, inplace  = True)
+    print(fourstreams_s.head())
+    print(fourstreams_s.nunique())
+    print(fourstreams_s.info())
+    print(len(fourstreams_s))
+    print(fourstreams_s.isna().sum())
+    #we export it as a csv
+    fourstreams_s.to_csv('fourstreamsstate.csv', index=False)
+
+=======
+    
+<<<<<<< HEAD
+=======
+    
+
+
+
+
+>>>>>>> d6ea100a7 (state-level-script)
+>>>>>>> b8d59185e (state-level-script)
---
+++ data-processed/LUcompUncertLab-VAR_3streams/vaccination.py
@@ -0,0 +1,140 @@
+from cmath import nan
+from heapq import merge
+import pandas as pd
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sqlalchemy import column
+
+if __name__ == "__main__":
+
+    threestreamsdf__county = pd.read_csv('threestreams__county.csv.gz') 
+    threestreamsdf__state  = pd.read_csv('threestreams__state.csv.gz')
+
+    threestreamsdf = threestreamsdf__state.append(threestreamsdf__county)
+
+    
+    threestreamsdf1 = threestreamsdf
+    threestreamsdf1['location'] = threestreamsdf1['location'].astype(str)
+    threestreamsdf1['state_fip'] = threestreamsdf1['location'].str[:2]
+    threestreamsdf1['date'] = pd.to_datetime(threestreamsdf1['date'])
+    threestreamsdf1 = threestreamsdf1[(threestreamsdf1['date'] >= '2022-02-13') & (threestreamsdf1['date'] <= '2022-04-29')]
+    
+    threestreamsdf3 = threestreamsdf1.groupby(['state_fip','date']).sum()
+
+    vaccination_df= pd.read_csv('allVaccinationData.csv.gz')
+    vaccination_df = vaccination_df.loc[:, ["date","fips","recip_county", "recip_state", "series_complete_yes"]]
+    vaccination_df = vaccination_df[vaccination_df['fips']!= 'UNK']
+    vaccination_df['fips'] = vaccination_df['fips'].astype(int)
+
+    vaccination_df['state_fip'] = [str(x)[:2] for x in vaccination_df['fips']]
+    vaccination_df['date'] = pd.to_datetime(vaccination_df['date'])
+    
+    #We will have to change the names of the columns in vaccination_df_5 so that we have a single column
+    vaccination_df.rename(columns = {'date':'date','Recip_County':'location_name', 'fips':'location','series_complete_yes':'vac_count'}, inplace=True)
+    vaccination_df = vaccination_df[(vaccination_df['date'] >= '2022-02-13') & (vaccination_df['date'] <= '2022-04-29')]
+
+    # Note here. The below FIPS are likely not contained in the threestreams dataset.
+    # They will be removed upong merging the vaccine data with ThreeStreams (parth,tom)
+    #lis1 = ['UN','72','66',nan]
+    #vaccination_df = vaccination_df[~vaccination_df['state_fip'].isin(lis1)]
+
+    #importing the location csv
+    location_df = pd.read_csv('../../data-locations/locations.csv') 
+
+    location_df_state  = location_df[location_df['abbreviation'].notna()]
+    location_df_county = location_df[location_df['abbreviation'].isna()]
+    location_df_state = location_df_state[location_df_state['location']!= 'US']
+    
+    
+    location_df_county['location'] = location_df_county['location'].astype(int)
+    location_df_state['location'] = location_df_state['location'].astype(int)
+    location_df_county['state_fip'] = [str(x)[:2] for x in location_df_county['location']]
+    location_df_state['state_fip'] = [str(x)[:2] for x in location_df_state['location']]
+
+    location_final = pd.merge(location_df_county, location_df_state, how  = 'left', left_on=['state_fip'], right_on=['state_fip'])
+    location_final.rename(columns = {'location_x':'location','location_name_x':'location_name','abbreviation_y':'state_ab','location_name_y':'state','population_y':'state_pop','population_x':'county_pop'}, inplace  = True)
+
+    location_final.drop(['abbreviation_x','location_y'], inplace = True, axis = 1)
+    location_final['per_state_pop'] = location_final['county_pop']/location_final['state_pop']
+
+    # HARD CODE FOR VI.
+    # The Virgin Islands only has a single county and so we are hardocding in a row to location_final (parth,tom).
+    # VI_row = location_df.loc[location_df.location=="78"]
+    
+    temp1 = location_final[['state_ab','state_pop']]
+    print(temp1.groupby('state_pop')['state_ab'].unique())
+
+    vaccination_df_merge = vaccination_df
+    vaccination_df_merge['date'] = pd.to_datetime(vaccination_df_merge['date'])
+    vaccination_df_merge.rename(columns = {'date':'date','recip_county':'location_name', 'FIPS':'location','Series_Complete_Yes':'Vacc_count','recip_state':'state_ab'}, inplace=True)
+    vaccination_df_merge = vaccination_df_merge[(vaccination_df_merge['date'] >= '2022-02-13') & (vaccination_df_merge['date'] <= '2022-04-29')]
+
+    vaccination_df_merge = vaccination_df_merge.merge( location_final, on = ["location", "location_name", "state_ab","state_fip"] )
+
+    #print(vaccination_df_merge[vaccination_df_merge.isna().any(axis=1)])
+    #print(vaccination_df_merge[vaccination_df_merge['vac_count'].isna()])
+
+    vaccination_df_state = vaccination_df_merge.groupby(['state_ab','date'], as_index=False)['vac_count'].sum()
+    
+    vaccination_data_and_census_data_at_state_level = pd.merge(vaccination_df_state, location_final, how = 'left',left_on=['state_ab'], right_on=['state_ab'])
+    vaccination_data_and_census_data_at_state_level = vaccination_data_and_census_data_at_state_level.rename(columns = {'vac_count':'state_vac_count'})
+
+    vaccination_data_and_census_data_at_state_level = vaccination_data_and_census_data_at_state_level[ ["date","location","state_vac_count"] ]
+
+    vaccination_date_at_state_and_county = vaccination_df_merge.merge( vaccination_data_and_census_data_at_state_level, on  = ["date","location"] )
+
+    merge_4 = vaccination_date_at_state_and_county
+    
+    merge_4['cal_vac'] = merge_4['state_vac_count']*(merge_4['county_pop']/merge_4['state_pop'])
+    merge_4['vac_count'].fillna(merge_4['cal_vac'], inplace = True)
+
+    #after this we will merge with the threestreams data
+    threestreamsdf_c = pd.read_csv('threestreams__county.csv.gz')
+    threestreamsdf_c['location'] = threestreamsdf_c['location'].astype(str)
+    threestreamsdf_c['state_fip'] = threestreamsdf_c['location'].str[:2]
+    threestreamsdf_c['date'] = pd.to_datetime(threestreamsdf_c['date'])
+    threestreamsdf_c = threestreamsdf_c[(threestreamsdf_c['date'] >= '2022-02-13') & (threestreamsdf_c['date'] <= '2022-04-29')]
+
+
+    #now we merge
+    threestreamsdf_c['location'] = threestreamsdf_c['location'].astype(int)
+    fourstreams = pd.merge(threestreamsdf_c, merge_4, how = 'left',left_on=['date','location','state_fip'], right_on=['date','location','state_fip'])
+    
+    #print(fourstreams.head())
+    #print(fourstreams.info())
+    #print(fourstreams.nunique())
+    #print(len(fourstreams))
+    #print(fourstreams.isna().sum())
+    #checking for the state fips
+    #print(threestreamsdf_c['state_fip'].unique())
+    #print(merge_4['state_fip'].unique())
+    #there are no fips in merge_4 for fips 60, 61,80, 81 and 90 in threestreams
+    #maybe we can check for nan values after removing them
+    lis1 = ['60','61','80','81','90']
+    fourstreams = fourstreams[~fourstreams['state_fip'].isin(lis1)]
+    print(fourstreams.head())
+    print(fourstreams.info())
+    print(fourstreams.nunique())
+    print(len(fourstreams))
+    print(fourstreams.isna().sum())
+
+    #please note that we still have a lot of nan values that needs to be populated and inspite of
+    #taking on 2021 values we are still getting 40k Nan Values 
+    #Now we remove all the unnecassary set of columns
+    #trying to work on the Naan Values
+    #post chaning the date to 2021
+
+    #
+    fourstreams = fourstreams.loc[:,["date","location","location_name_x","county_cases","state_deaths","state_hosps","state_ab","vac_count","state_fip"]]
+    fourstreams.rename(columns = {'county_cases':'cases','state_deaths':'deaths','state_hosps':'hosps'}, inplace  = True)
+    print(fourstreams.head())
+    print(fourstreams.info())
+    print(fourstreams.nunique())
+    print(len(fourstreams))
+    print(fourstreams.isna().sum())
+    #store the fourstreams as a csv file
+    fourstreams.to_csv('fourstreamscounty.csv', index=False)
+    #
