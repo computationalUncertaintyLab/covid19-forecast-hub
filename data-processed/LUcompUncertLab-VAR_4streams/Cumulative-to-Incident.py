18b34d04857e920dec6a8d9ed2ddf8a7ad2af9f7
Parth github <98370984+parthdg420@users.noreply.github.com>
2022-07-27 17:13:47 -0400

    Converting cumulative to incident for vac_count

---
+++ data-processed/LUcompUncertLab-VAR_4streams/Cumulative-to-Incident.py
@@ -0,0 +1,26 @@
+#parth,mcandrew
+
+import sys
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import validation_curve
+from sympy import Le
+from itertools import chain
+
+if __name__ == "__main__":
+    _4streams_df = pd.read_csv('_4streams.csv.gz')
+    cumulative_vac_count = _4streams_df[['date','location_name','vac_count']]
+    list_values = []
+    for x in cumulative_vac_count['location_name'].unique():
+        temp1 = cumulative_vac_count[cumulative_vac_count['location_name'] == x]
+        vac_count_1 = temp1['vac_count']
+        vac_count_1 = vac_count_1.reset_index(drop = True)
+        c = np.diff(vac_count_1)
+        a = vac_count_1[0]
+        d = list(c)
+        final_list = list(chain.from_iterable([[a], d]))
+        list_values = list_values + final_list
+
+_4streams_df.drop('vac_count', axis = 1, inplace = True)
+_4streams_df['vac_count'] = list_values
+_4streams_df.to_csv('_4streams.csv.gz')
